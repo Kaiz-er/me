49fa2ed4d82fc05ea95787b5e5e8761589a7ecde
Kaiz-er <kzercube@gmail.com>
2025-08-10 16:16:26 +0800

    Add GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
   title: "Kaizer",
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`antialiased bg-gray-1`}>{children}</body>
+      <GoogleAnalytics gaId="G-8L41G49WPR" />
     </html>
   );
 }
